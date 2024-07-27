806fe8955bf9fcd0706aa03ebda257224db30036
compusta.az <tarlanshukur@gmail.com>
2024-07-27 17:35:28 +0400

    numpy

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a simpe module file
+
+def sum_operations(a,b):
+    """Add to given numbers"""
+    return a + b
+
+
+print(sum_operations(30,40))
+
+print("Verilen iki sayinin toplami:", sum_operations(int(input("Bir sayi giriniz:"))))
+      
+
